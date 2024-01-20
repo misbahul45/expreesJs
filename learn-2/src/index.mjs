b3f0b5b6a452ba66b655ac85fbeb5ccaafedbe4c
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-01-21 00:16:52 +0700

    push

--- learn-2/src/index.mjs
+++ learn-2/src/index.mjs
@@ -2,32 +2,11 @@ import express from "express"
 import session from  "express-session"
 import cookieParser from "cookie-parser"
 import passport from "passport"
-import "./strategies/local-strategy.mjs"
-import userRouter from "./router/userRoute.mjs"
+
 
 const app=express()
 
 app.use(express.json())
-app.use(session({
-    secret:"takin123",
-    saveUninitialized:false,
-    resave:false,
-    cookie:{
-        maxAge:600000*60,
-    }
-}))
-app.use(passport.initialize())
-
-app.post('/api/auth',
-    passport.authenticate("local",{
-        failureRedirect: '/login-failed',
-        failureFlash: true 
-    }),
-    (req, res)=>{
-        
-    }
-)
-app.use(userRouter)
 
 
 app.listen(3001,()=>{
