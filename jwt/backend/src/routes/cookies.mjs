2519a4962c91fba9c43efda24a41bf0951045c41
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-02-11 19:36:58 +0700

    lontong

--- jwt/backend/src/routes/cookies.mjs
+++ jwt/backend/src/routes/cookies.mjs
@@ -5,12 +5,13 @@ const cookieRouter=Router()
 cookieRouter.get('/set-cookies',(req,res)=>{
     //res.setHeader('set-cookie','newUser=true')
     res.cookie('newUser', false)
-    res.cookie('isEmplooye',true, {})
+    res.cookie('isEmplooye',true, {maxAge:1000*60*60*24, secure:true, httpOnly:true})
     return res.send({ msg:'You got the cookie' })
 })
 
 cookieRouter.get('/read-cookies',(req,res)=>{
-    
+    const cookies=req.cookies;
+    res.json(cookies) 
 })
 
 export default cookieRouter
