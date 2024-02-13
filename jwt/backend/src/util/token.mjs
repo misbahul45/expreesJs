cec39faf26d2b3ac5c5c4778350c2473c2ea8d67
misbahul45 <misbahulmuttaqin395@gmail.com>
2024-02-13 13:29:41 +0700

    push

--- jwt/backend/src/util/token.mjs
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken"
-export const maxAge=
-3*24*60*60
-export const createToken=(id)=>{
-    return jwt.sign({ id }, 'misbahul secret', { 
-        expiresIn:maxAge
-    })
-}
